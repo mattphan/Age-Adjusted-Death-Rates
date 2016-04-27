00a7b3e529a58641f0a76f49e6ecc21e6f09f358
mattphan <mphan@me.com>
2016-04-27 02:58:10 -0400

    adding data and new functions for alzheimer, cancer, and top10 deaths

---
+++ R/top10.R
@@ -0,0 +1,40 @@
+top10 = function(x)
+{
+  
+  library(ggplot2)
+  library(dplyr)
+  
+  us <- map_data("state")
+  
+  AADR.DF <- read.csv("data/AADR-all.csv")
+  
+  top10wYear <- subset(AADR.DF, YEAR == x, select=c (YEAR, region, DEATHS, AADR))
+  
+  top10wYear <- top10wYear %>% 
+    mutate(region=tolower(region))
+  
+  gg <- ggplot()
+  
+  gg <- gg + geom_map(data=us, map=us,
+                      aes(x=long, y=lat, map_id=region),
+                      fill="#ffffff", color="#ffffff", size=0.15)
+  
+  gg <- gg + geom_map(data=top10wYear, map=us,
+                      aes(fill=DEATHS, map_id=region),
+                      color="#ffffff", size=0.15)
+  
+  gg <- gg + labs(x=NULL, y=NULL) + ggtitle("Top 10 Deaths")
+  
+  gg <- gg + coord_map("albers", lat0 = 39, lat1 = 45)
+  
+  gg <- gg + theme(panel.border = element_blank())
+  
+  gg <- gg + theme(panel.background = element_blank())
+  
+  gg <- gg + theme(axis.ticks = element_blank())
+  
+  gg <- gg + theme(axis.text = element_blank())
+  
+  gg
+  
+}
