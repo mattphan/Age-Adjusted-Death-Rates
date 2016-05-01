c1ae3b95acc508cfecb0ba9b2a97c09b57f0f4bb
mattphan <mphan@me.com>
2016-05-01 15:43:38 -0400

    created description files for datasets used

---
+++ R/alzheimer-desc.R
@@ -0,0 +1,20 @@
+#' Number of deaths for alzheimers in the US from 1999-2013
+#' 
+#' A dataset containing the Age Adjusted Death Rates and number of 
+#' deaths for alzheimers listed by region and year. The variables
+#' are as follows:
+#' 
+#' \itemize{
+#'  \item YEAR. The year in which the deaths occurred
+#'  \item CAUSE_NAME. The cause of deaths
+#'  \item region. The state in which the deaths occurred
+#'  \item DEATHS. The number of Deaths via state and year
+#'  \item AADR. Age Adjusted Death Rates via state and year
+#' }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name AADR-alzheimer
+#' @usage data(AADR-alzheimer)
+#' @format Comma Separate Value file with 765 rows and 5 variables.
+NULL
